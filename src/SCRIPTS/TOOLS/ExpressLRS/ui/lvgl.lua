5569ebf15e16999fdf901059d3a02e3235651ba3
Julius Jurgelenas <julius@jurgelenas.lt>
2026-03-04 12:47:31 +0200

    move device name to the bottom

--- src/SCRIPTS/TOOLS/ExpressLRS/ui/lvgl.lua
+++ src/SCRIPTS/TOOLS/ExpressLRS/ui/lvgl.lua
@@ -1038,10 +1038,6 @@ function UI.build()
       end
     end
   else
-    if currentFolder == nil then
-      UI.createInfoRow(fieldContainer, { name = "Device", value = Protocol.deviceName or "Searching..." })
-    end
-
     local fieldsInFolder = Protocol.getFieldsInFolder(currentFolder)
     local FOLDERS_PER_ROW = 2
     if IS_NARROW then
@@ -1092,6 +1088,10 @@ function UI.build()
 
     if currentFolder == nil and Protocol.deviceIsELRS_TX then
       UI.createInfoRow(fieldContainer, { name = "Lua script version", value = VERSION })
+    end
+
+    if currentFolder == nil then
+      UI.createInfoRow(fieldContainer, { name = "Device name", value = Protocol.deviceName or "Searching..." })
     end
 
     if currentFolder == nil and #Protocol.devices > 1 and not Navigation.hasDeviceEntry() then
