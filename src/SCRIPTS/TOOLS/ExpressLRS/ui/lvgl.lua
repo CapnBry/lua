1366aea5fedbe2c43615fc8286688e9a53225045
Julius Jurgelenas <julius@jurgelenas.lt>
2026-03-06 18:24:35 +0200

    fix code style

--- src/SCRIPTS/TOOLS/ExpressLRS/ui/lvgl.lua
+++ src/SCRIPTS/TOOLS/ExpressLRS/ui/lvgl.lua
@@ -733,7 +733,9 @@ function UI.createToggleRow(pg, field)
   pg:setting({
     w = lvgl.PERCENT_SIZE + 100,
     title = field.name,
-    visible = function() return not field.hidden end,
+    visible = function()
+      return not field.hidden
+    end,
     children = {
       {
         type = lvgl.BOX,
@@ -763,7 +765,9 @@ function UI.createToggleRow(pg, field)
               {
                 type = lvgl.LABEL,
                 y = lvgl.PAD_MEDIUM,
-                text = function() return field.unit or "" end,
+                text = function()
+                  return field.unit or ""
+                end,
               },
             },
           },
@@ -781,7 +785,9 @@ function UI.createChoiceRow(pg, field)
     w = lvgl.PERCENT_SIZE + 100,
     title = field.name,
     visible = function()
-      if field.hidden then return false end
+      if field.hidden then
+        return false
+      end
       if field.values ~= valuesRef then
         valuesRef = field.values
         if choiceWidget then
@@ -826,7 +832,9 @@ function UI.createChoiceRow(pg, field)
         {
           type = lvgl.LABEL,
           y = lvgl.PAD_MEDIUM,
-          text = function() return field.unit or "" end,
+          text = function()
+            return field.unit or ""
+          end,
         },
       },
     },
@@ -879,7 +887,9 @@ function UI.createNumberRow(pg, field)
               {
                 type = lvgl.LABEL,
                 y = lvgl.PAD_MEDIUM,
-                text = function() return field.unit or "" end,
+                text = function()
+                  return field.unit or ""
+                end,
               },
             },
           },
@@ -894,7 +904,9 @@ function UI.createNumberRow(pg, field)
   pg:setting({
     w = lvgl.PERCENT_SIZE + 100,
     title = field.name,
-    visible = function() return not field.hidden end,
+    visible = function()
+      return not field.hidden
+    end,
     children = children,
   })
 end
@@ -905,12 +917,16 @@ function UI.createInfoRow(pg, field)
       type = lvgl.SETTING,
       w = lvgl.PERCENT_SIZE + 100,
       title = field.name,
-      visible = function() return not field.hidden end,
+      visible = function()
+        return not field.hidden
+      end,
       children = {
         {
           type = lvgl.LABEL,
           x = LABEL_PCT,
-          text = function() return field.value or "" end,
+          text = function()
+            return field.value or ""
+          end,
         },
       },
     },
@@ -923,7 +939,9 @@ function UI.createStringRow(pg, field)
       type = lvgl.SETTING,
       w = lvgl.PERCENT_SIZE + 100,
       title = field.name,
-      visible = function() return not field.hidden end,
+      visible = function()
+        return not field.hidden
+      end,
       children = {
         {
           type = lvgl.TEXT_EDIT,
@@ -947,8 +965,12 @@ end
 
 function UI.createFolderWidget(pg, field, width)
   pg:button({
-    text = function() return field.name or "" end,
-    visible = function() return not field.hidden end,
+    text = function()
+      return field.name or ""
+    end,
+    visible = function()
+      return not field.hidden
+    end,
     w = width or (lvgl.PERCENT_SIZE + 100),
     h = lvgl.UI_ELEMENT_HEIGHT * 2,
     press = function()
@@ -963,10 +985,14 @@ function UI.createCommandWidget(pg, field)
     flexFlow = lvgl.FLOW_COLUMN,
     align = CENTER,
     borderPad = { top = lvgl.PAD_TINY, bottom = lvgl.PAD_TINY },
-    visible = function() return not field.hidden end,
+    visible = function()
+      return not field.hidden
+    end,
   })
   wrapper:button({
-    text = function() return field.name or "" end,
+    text = function()
+      return field.name or ""
+    end,
     w = lvgl.PERCENT_SIZE + 99,
     press = function()
       Protocol.handleCommandSave(field)
